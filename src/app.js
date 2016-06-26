df9b501613f0f7da1a45221df658e6732b47f569
tomahawk <tmutunhire@gmail.com>
2016-06-27 00:39:55 +0200

    preconfigure testing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.css';
 import './app.css';
 import styles from './styles.module.css';
 
 const App = React.createClass({
   render: function() {
-    return (<div className={styles.wrapper}>Said I loved you but I lied!</div>);
+    return (
+      <div className={styles.wrapper}>
+        <h1>
+        <i className="fa fa-star"></i>
+        Environment: {__NODE_ENV__}</h1>
+        Said I loved you but I lied!
+      </div>);
   }
 });
 
