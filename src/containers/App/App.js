fa19f08d0e5538bbff8d4668f3db9b334bf0a851
tomahawk <tmutunhire@gmail.com>
2016-06-27 03:37:28 +0200

    add and fix routing

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,15 +4,22 @@ import { Router } from 'react-router';
 
 import styles from './styles.module.css';
 
-class App extends React.COmponent {
+class App extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
 
   static propTypes = {
     routes: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired
+    history: PropTypes.element.isRequired
   };
 
   get content() {
-    return (<Router history={this.props.history} />);
+    return (
+      <Router
+      routes={this.props.routes}
+      history={this.props.history} />
+    );
   }
 
   render() {
@@ -24,4 +31,4 @@ class App extends React.COmponent {
   }
 }
 
-module.exports = App;
+export default App;
