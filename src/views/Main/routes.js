b60cdeceab69a355a0cae7c935c1a91036b07625
tomahawk <tmutunhire@gmail.com>
2016-07-11 13:22:08 +0200

    add fish route

--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -2,9 +2,22 @@ import React from 'react';
 import {Route} from 'react-router';
 import Container from './Container';
 
+class Fish extends React.Component {
+  render() {
+    return(
+      <div>
+        I am a Fish
+      </div>
+    );
+  }
+}
+
 export const makeMainRoutes = () => {
   return (
-    <Route path="/" component={Container} />
+    <div>
+      <Route path="/" component={Container} />
+      <Route path="/dogs" component={Fish} />
+    </div>
   );
 }
 
