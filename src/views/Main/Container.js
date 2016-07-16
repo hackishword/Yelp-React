977bc73187b382e30553f643fc0084166b0c4005
tomahawk <tmutunhire@gmail.com>
2016-07-16 03:47:02 +0200

    mount the map

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Map, {GoogleApiWrapper} from 'google-maps-react';
 
 export class Container extends React.Component {
+
+  onReady(mapProps, map) {
+    //  when the map is ready and mounted
+  }
+
   render() {
     return (
       <div>
